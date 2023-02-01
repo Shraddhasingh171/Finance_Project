7404c09801c59eec342b2beac4f0e428011a03e2
Rishabh Shukla <rs515666@gmail.com>
2023-02-02 02:42:54 +0530

    Added forgot password page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
+// ONBOARDING SCREENS :
 import 'package:app/screens/onboarding/splash_screen.dart';
 import 'package:app/screens/onboarding/onboarding1.dart';
 import 'package:app/screens/onboarding/onboarding3.dart';
 import 'package:app/screens/onboarding/onboarding4.dart';
 import 'package:app/screens/onboarding/onboarding5.dart';
+
+// SIGN-UP SCREENS :
+import 'package:app/screens/signup/forgot_password.dart';
 
 void main()
 {
@@ -20,6 +25,9 @@ class MyApp extends StatelessWidget
     return MaterialApp(
       title: "BeeWiser | Manage Your Finance",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true
+      ),
 
       initialRoute: "/",
       routes: {
@@ -28,6 +36,8 @@ class MyApp extends StatelessWidget
           "/onboarding3" : (context) => const Onboarding3(),
           "/onboarding4" : (context) => const Onboarding4(),
           "/onboarding5" : (context) => const Onboarding5(),
+
+          "/forgot-password" : (context) => const ForgotPassword()
       }
     );
   }
